b426b2ae17dfc3622d3f2d7cc9cd16089ff9f15e
tanimu <tanimu@ht.sfc.keio.ac.jp>
2019-08-21 16:50:04 +0900

    Add parallel execution script

---
+++ parallel_exec.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 0 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 1 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 2 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 3 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 4 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 5 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 6 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 7 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 8 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 9 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 10 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 11 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 12 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 13 -c 1 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 14 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 15 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 16 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 17 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 18 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 19 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 20 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 21 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 22 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 23 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 24 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 25 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 26 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 27 -c 2 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 28 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 29 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 30 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 31 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 32 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 33 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 34 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 35 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 36 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 37 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 38 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 39 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 40 -c 3 &
+nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 41 -c 3 &
+# nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 42 -c 1 &
+# nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 43 -c 1 &
+# nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 44 -c 1 &
+# nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 45 -c 1 &
+# nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 46 -c 1 &
+# nvidia-docker run --rm -v $GITHUB/tanimutomo/DeepTextures:/workspace texsyn python example.py -t TIN -s 47 -c 1 &
+
+echo "Running processes..."
+
+wait
+
+echo "All processes have done"
